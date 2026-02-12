e53bf02f41637679213c7e9182d292ac4ceca36a
Natallie <amadeunatallie@gmail.com>
2026-02-12 16:58:09 -0300

    feat: atualizar estrutura do projeto, adicionar componentes Link, SocialLink e Rodape, e ajustar o componente Perfil

--- 03-devlinks/src/App.jsx
+++ 03-devlinks/src/App.jsx
@@ -1,28 +1,36 @@
 
 import './App.css'
-import Perfil from './assets/components/perfil/Perfil'
+import Link from './assets/components/Link/Link';
+import Perfil from './assets/components/Perfil/Perfil';
+import SocialLink from './assets/components/SocialLink/SocialLink';
+import Rodape from './assets/components/Rodape/Rodape';
+
 
 function App() {
 
   return (
-   <div id= 'App'>
-<Perfil
+   <div id= "App">
+ <Perfil fotoPerfil={"https://placehold.co/100"}>Natallie</Perfil>
 
 
-    
-   <div className='perfil'>
-   <img src="https://placehold.co/200x200" alt="" />
-   <p>@SeuNome</p>
-   </div>
-   <div className="switch">
-    botão switch 
+       <div className="switch">botão switch</div>
+
+      <ul>
+        <Link url={""}>Inscreva-se</Link>
+        <Link url={""}>Minha Playlist</Link>
+        <Link url={""}>Me pague um lanche</Link>
+        <Link url={""}>Conheça o curso de DEV</Link>
+      </ul>
+
+      <div className='socialLinks'></div>
+       <SocialLink url={"https://github.com"} icon = {"logo-github"} />
+       <SocialLink url={"https://instagram.com"} icon = {"logo-instagram"} />
+       <SocialLink url={"https://youtube.com"} icon = {"logo-youtube"} />
+       <SocialLink url={"https://br.linkedin.com/"} icon = {"logo-linkedin"} />
+       
+      <Rodape>Natallie</Rodape>
     </div>
-   <div className='links'></div>
-   <div className='socialLinks'></div>
-   <div className='rodape'></div>
-   </div> 
-
-  )
+  );
 }
 
-export default App
+export default App;
