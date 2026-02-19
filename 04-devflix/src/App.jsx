f520a9d60a078bcddb8d04dc46c328cb8cb03659
Natallie <amadeunatallie@gmail.com>
2026-02-19 11:21:18 -0300

    feat: adicionar logo, barra de pesquisa e rodapé ao App; substituir favicon

--- 04-devflix/src/App.jsx
+++ 04-devflix/src/App.jsx
@@ -1,10 +1,31 @@
 import "./App.css";
 
+import logo from "./assets/devflix.png";
+import lupa from "./assets/search.svg";
+import Rodape from "./components/Rodape/Rodape";
+
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div id="App">
+      <img
+        id="logo"
+        src={logo}
+        alt="Logotipo do serviço de streaming DEVFLIX com letras vermelhas em fundo preto, representando uma plataforma de entretenimento digital."
+      />
 
-export default App
+      <div className="search">
+        <input type="text" placeholder="Pesquise por filmes e séries..." />
+        <img
+          role="button"
+          src={lupa}
+          onClick={(e) => e.stopPropagation}
+          alt="Botão de ação para pesquisa!"
+        />
+      </div>
 
+      <Rodape>Natallie </Rodape>
+    </div>
+  );
+};
+
+export default App;
