ea477f6204ffcf7ed2ee39cd19fb172a3e441895
Natallie <amadeunatallie@gmail.com>
2026-02-19 08:36:43 -0300

    feat: add Link, Perfil, Rodape, SocialLink, and Switch components with styles
    - Created Link component for navigation links with styling.
    - Added Perfil component to display user profile with image and Implementado o componente Rodape para o rodapé com texto personalizável.
    Desenvolvido o componente SocialLink para ícones de redes sociais.
    Introduzido o componente Switch para alternar temas com animações.
    Adicionados módulos CSS correspondentes para cada componente para gerenciar estilos.

--- 03-devlinks/src/App.jsx
+++ 03-devlinks/src/App.jsx
@@ -1,45 +1,41 @@
-
-import './App.css'
-import Link from './assets/components/Link/Link';
-import Perfil from './assets/components/Perfil/Perfil';
-import SocialLink from './assets/components/SocialLink/SocialLink';
-import Rodape from './assets/components/Rodape/Rodape';
-import Switch from './assets/components/Switch/Switch';
-import { useState } from 'react';
-
+import { useState } from "react";
+import "./App.css";
+import Link from "./components/Link/Link";
+import Perfil from "./components/Perfil/Perfil";
+import Rodape from "./components/Rodape/Rodape";
+import SocialLink from "./components/SocialLink/SocialLink";
+import Switch from "./components/Switch/Switch";
+import foto from "./assets/Hailey.avif"
 
 function App() {
-const [isLight, setIsLight] = useState(true);
+  const [isLight, setIsLight] = useState(true);
 
-const troca = () => {
-  setIsLight(!isLight);
-}
-
-
+  const troca = () => {
+    setIsLight(!isLight);
+  };
 
   return (
-   <div id= "App" className={isLight ? "light" : ""}>
- <Perfil fotoPerfil={"https://placehold.co/100"}>Natallie</Perfil>
+    <div id="App" className={isLight ? "light" : ""}>
+      <Perfil fotoPerfil={foto}>Hailey Bieber</Perfil>
 
-<Switch troca={troca} isLight={isLight} />
+      <Switch troca={troca} isLight={isLight} />
 
-<div id= "Link">
-      <ul>
-        <Link url={""}>Inscreva-se</Link>
-        <Link url={""}>Minha Playlist</Link>
-        <Link url={""}>Me pague um lanche</Link>
-        <Link url={""}>Conheça o curso de DEV</Link>
-      </ul>
+      <div id="Link">
+        <ul>
+          <Link url={""}>Inscreva-se</Link>
+          <Link url={""}>Minha playlist</Link>
+          <Link url={""}>Me pague um café!</Link>
+          <Link url={""}>Conheça o Curso DEV</Link>
+        </ul>
       </div>
 
-      <div id='SocialLinks'>
-       <SocialLink url={"https://github.com"} icon = {"logo-github"} />
-       <SocialLink url={"https://instagram.com"} icon = {"logo-instagram"} />
-       <SocialLink url={"https://youtube.com"} icon = {"logo-youtube"} />
-       <SocialLink url={"https://br.linkedin.com/"} icon = {"logo-linkedin"} />
-       </div>
-
-      <Rodape>Natallie</Rodape>
+      <div id="SocialLinks">
+        <SocialLink url={"https://github.com"} icon={"logo-github"} />
+        <SocialLink url={"https://instagram.com"} icon={"logo-instagram"} />
+        <SocialLink url={"https://youtube.com"} icon={"logo-youtube"} />
+        <SocialLink url={"https://br.linkedin.com/"} icon={"logo-linkedin"} />
+      </div>
+      <Rodape>ProfCastello</Rodape>
     </div>
   );
 }
