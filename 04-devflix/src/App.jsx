7102554e7a6d24e574212f9cdcad72b519df6e23
Natallie <amadeunatallie@gmail.com>
2026-02-20 17:05:09 -0300

    feat: adicionar modal de descrição de filme e refatorar pesquisa de filmes

--- 04-devflix/src/App.jsx
+++ 04-devflix/src/App.jsx
@@ -9,6 +9,7 @@ import MovieCard from "./components/MovieCard/MovieCard";
 
 const App = () => {
   const [movies, setMovies] = useState([]);
+  const [search, setSearch] = useState("");
 
   //Utilizando uma CHAVE de API do arquivo .env
   const apiKey = import.meta.env.VITE_OMDB_API_KEY;
@@ -24,7 +25,7 @@ const App = () => {
   };
 
   useEffect(() => {
-    searchMovies("Harry Potter");
+    searchMovies("Hulk"); // termo para pesquina ao carregar o site
   }, []);
 
   return (
@@ -36,21 +37,30 @@ const App = () => {
       />
 
       <div className="search">
-        <input type="text" placeholder="Pesquise por filmes" />
-        <img src={lupa} alt="Botão de ação para pesquisa!" />
+        <input
+          onKeyDown={(e) => e.key === "Enter" && searchMovies(search)}
+          onChange={(e) => setSearch(e.target.value)}
+          type="text"
+          placeholder="Pesquise por filmes"
+        />
+        <img
+          onClick={() => searchMovies(search)}
+          src={lupa}
+          alt="Botão de ação para pesquisa!"
+        />
       </div>
 
       {movies?.length > 0 ? (
         <div className="container">
           {movies.map((movie, index) => (
-            <MovieCard key={index} {...movie} />
+            <MovieCard key={index} {...movie} apiUrl={apiUrl} />
           ))}
         </div>
       ) : (
-        <h2>😓 Filme não encontrado 😓</h2>
+        <h2 className="empty">😢 Filme não encontrado 😢</h2>
       )}
 
-      <Rodape link={"https://github.com/NatallieAmadeu22"}>Natallie</Rodape>
+      <Rodape link={"https://github.com/ProfCastello"}>Natallie</Rodape>
     </div>
   );
 };
